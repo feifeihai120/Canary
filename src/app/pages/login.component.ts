6de0a096eca85c854d50b607ec4225ef8fc624a6
wangshang <wangshang.a@qq.com>
2017-04-21 08:34:55 +0800

    handle user login

--- src/app/pages/login.component.ts
+++ src/app/pages/login.component.ts
@@ -9,61 +9,67 @@ import { UserService } from '../service/user.service'
 import { User } from '../service/user'
 
 @Component({
-  moduleId: module.id,
-  selector: 'login',
-  templateUrl: 'login.component.html'
+	moduleId: module.id,
+	selector: 'login',
+	templateUrl: 'login.component.html'
 })
 export class LoginComponent implements OnInit {
 
-  private loginUser: LoginUser
+	private loginUser: LoginUser = new LoginUser()
 
-  private message: string = ''
+	private message: string = ''
 
-  private showMessage: boolean
+	private showMessage: boolean = false
 
-  @SessionStorage() private currUser: User = new User()
+	@SessionStorage() private currUser: User = new User()
 
-  constructor(
-    private router: Router,
-    private userService: UserService
-  ) { }
+	constructor(
+		private router: Router,
+		private userService: UserService
+	) { }
 
-  ngOnInit() {
-    this.loginUser = new LoginUser()
-    this.showMessage = false
-  }
+	ngOnInit() { }
 
-  login() {
-    // console.log('login user: ')
-    // console.log(this.loginUser)
-    this.check()
-    this.setLoginFlag()
-    this.userService.login(this.loginUser)
-      .then(user => {
-        this.currUser = user
-        setTimeout(2000)
-        this.router.navigateByUrl("/meeting/meetingList")
-      })
-  }
+	login() {
+		// console.log('login user: ')
+		// console.log(this.loginUser)
+		if (this.check()) {
+			this.setLoginFlag()
+			this.userService.login(this.loginUser)
+				.then(user => {
+					this.currUser = user
+					setTimeout(2000)
+					this.router.navigateByUrl("/meeting/meetingList")
+				})
+				.catch(error => {
+					console.log(error)
+					this.showMessage = true
+					this.message = error.message
+				})
+		}
+	}
 
-  /**
-   * 校验 用户输入的 数据是否合法
-   */
-  check() {
-    if (this.loginUser.domain === null || this.loginUser.domain.length <= 0) {
-      this.showMessage = true
-      this.message = "请输入正确的用户名/手机号/邮箱"
-    }
-    if (this.loginUser.password === null || this.loginUser.password.length <= 0) {
-      this.showMessage = true
-      this.message = '请输入密码'
-    }
-  }
+	/**
+	 * 校验 用户输入的 数据是否合法
+	 */
+	check(): boolean {
+		if (this.loginUser.domain === null || this.loginUser.domain.length <= 0) {
+			this.showMessage = true
+			this.message = "请输入正确的用户名/手机号/邮箱"
+			return false
+		}
+		if (this.loginUser.password === null || this.loginUser.password.length <= 0) {
+			this.showMessage = true
+			this.message = '请输入密码'
+			return false
+		}
+		return true
+	}
 
-  /**
-   * 根据用户 输入的 数据 判断用户的登录方式, 暂时现默认 用户名 登录
-   */
-  setLoginFlag() {
-    this.loginUser.flag = 1
-  }
+	/**
+	 * 根据用户 输入的 数据 判断用户的登录方式, 暂时现默认 用户名 登录
+	 */
+	setLoginFlag() {
+		this.loginUser.flag = 1
+	}
 }
